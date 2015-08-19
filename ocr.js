87f8bfb1912172012c0220bafe8accef5457c467
thanhacun <thanhacun@gmail.com>
2015-08-19 15:24:38 +0000

    Combine cookies and captcha, if captcha false, try again

--- ocr.js
+++ ocr.js
@@ -1,27 +1,21 @@
 //TODO: pure javascript
 (function(){
+    //it take lessthan 500 miliseconds to crack
     var internal = {};
     var tesseract = require('node-tesseract');
     var request = require('request');
     var fs = require('fs');
     
     exports.crack = internal.crack = function(captcha_url, callback){
-      var start = new Date().getTime();
       request(captcha_url).pipe(fs.createWriteStream(__dirname + '/captcha.png')).on('finish', function(){
          tesseract.process(__dirname + '/captcha.png', {'psm':7}, function(err, text){
              if(err){
                  callback(false);
              } else {
-                 var end = new Date().getTime();
-                 console.log(text.slice(0,5), 'Time to crack: ', end - start);
                  callback(text.toLowerCase().slice(0,5));
              }
          });
       });
-    };
-    
-    exports.tong = internal.tong = function(a, b) {
-        return a + b;
     };
     
     //Running standalone mode
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var cheerio = require('cheerio');
-var mst = require('./tcmst.js');
+var phantom = require('phantom');
+//var mst = require('./tcmst.js');
 var ocr = require('./ocr.js');
+//var cook_capt = require('./cookie.js');
+
 
 var app = express();
 app.set('views', __dirname + '/client/views');
@@ -13,22 +16,53 @@ app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 //global variables
+var homepage = 'http://tracuunnt.gdt.gov.vn';
+var url = 'http://tracuunnt.gdt.gov.vn/tcnnt/mstdn.jsp';
 var captcha;
 var cookies;
+var capt_correct = true;
+var tick = 0;
+var pageLoading = false;
+var count = 0;
 
-//using phantom and phantomjs
-app.get('/TODO/:mst', function(req, res){
-  //because cracking captcha is not always correct
-  //TODO will try a few times
-  console.log('Total queries:', count++);
-  mst.tcmst(req.params.mst, function(data){
-    res.json(data);
-    if (data.captcha){
-      console.log(data);
-    }
-    //res.end();
-  });
-});
+var cook_capt = function(){
+    phantom.create(function(ph){
+       ph.createPage(function(page){
+            //page settings
+            page.set('onLoadStarted', function(){
+              console.log('Loading...');
+              pageLoading = true;
+            });
+            page.set('onLoadFinished', function(){
+              console.log('Finised.');
+              pageLoading = false;
+            });
+            //helper functions
+            //get captcha
+            function get_capt(){
+              page.evaluate(function(){
+                return $('img').attr('src');
+              }, function(captcha_url){
+                  ocr.crack(homepage + captcha_url, function(text){
+                    captcha = text;
+                    console.log(captcha, homepage + captcha_url);
+                    capt_correct = true;
+                    ph.exit();
+                  });
+                });
+            }
+            page.open(url, function(status){
+                page.get('cookies', function(cookies_obj){
+                    cookies = cookies_obj[0].name + '=' + cookies_obj[0].value + '; ' + cookies_obj[1].name + '=' + cookies_obj[1].value;
+                });
+                get_capt();
+                //TODO: make sure captcha correct
+                });
+            });
+       });  
+};
+
+cook_capt();
 
 app.get('/captcha/:uid', function(req, res){
   var captcha_url = 'http://tracuunnt.gdt.gov.vn/tcnnt/captcha.png?uid=' + req.params.uid;
@@ -49,13 +83,10 @@ app.route('/mst')
     res.render('index');
   });
 
-
-var count = 0;
 app.get('/mst/:mst', function(req, res){
   //variables
-  var homepage = 'http://tracuunnt.gdt.gov.vn';
-  var url = 'http://tracuunnt.gdt.gov.vn/tcnnt/mstdn.jsp';
-  var query = '?action=action&mst=' + req.params.mst + '&captcha=';
+  var mst = req.params.mst;
+  var query = '?action=action&mst=' + mst + '&captcha=';
   var options = {
     uri: url + query + captcha,
     headers: {
@@ -65,8 +96,8 @@ app.get('/mst/:mst', function(req, res){
   };
   
   //result
-  var mst, ten, diachi, thanhpho, quan, phuong,  trangthai, ketqua;
-  var json = {mst: "", ten: "", diachi: "", thanhpho: "", quan: "", phuong: "", trangthai: "", ketqua:""};
+  //var mst, ten, diachi, thanhpho, quan, phuong,  trangthai, ketqua;
+  var json = {mst: "", ten: "", diachi: "", thanhpho: "", quan: "", phuong: "", trangthai: "", ketqua:false, captcha:false};
   
   //open a browser to get cookie and capcha first
   request(options, function(error, response, body){
@@ -74,42 +105,33 @@ app.get('/mst/:mst', function(req, res){
        $ = cheerio.load(body);
       //make sure ta_border exists
       if ($('.ta_border').length > 0){//captcha correct
-        ketqua = true;
-        json.ketqua = ketqua;
+        json.captcha = true;
         if ($('.ta_border').find('tr').length > 2) {//having result
+          json.ketqua = true;
           var info = $('.ta_border tr').eq(1).find('td');
-          mst = $(info[1]).find('a').text();
-          json.mst = mst;
-          ten = $(info[2]).find('a').text();
-          json.ten = ten;
-          diachi = $(info[2]).find('a').attr('title').slice(16);
-          json.diachi = diachi;
-          trangthai = $(info[5]).find('a').attr('alt');
-          json.trangthai = trangthai;
+          json.mst = $(info[1]).find('a').text();
+          json.ten = $(info[2]).find('a').text();
+          json.diachi = $(info[2]).find('a').attr('title').slice(16);
+          json.trangthai = $(info[5]).find('a').attr('alt');
           //getting address detail
           //by request with id
           request({uri: url + '?action=action&id=' + mst}, function(error, response, body){
             //console.log(response.statusCode);
             if(!error && response.statusCode === 200){
               $ = cheerio.load(body);
-              thanhpho = $('.ta_border').find('tr').eq(4).find('td').eq(1).text();
-              json.thanhpho = thanhpho;
-              quan = $('.ta_border').find('tr').eq(5).find('td').eq(1).text();
-              json.quan = quan;
-              phuong = $('.ta_border').find('tr').eq(6).find('td').eq(1).text();
-              json.phuong = phuong;
-              res.json(json);
+              json.thanhpho = $('.ta_border').find('tr').eq(4).find('td').eq(1).text();
+              json.quan = $('.ta_border').find('tr').eq(5).find('td').eq(1).text();
+              json.phuong = $('.ta_border').find('tr').eq(6).find('td').eq(1).text();
+              res.jsonp(json);
               console.log(mst, count++, response.statusCode);
             }
           });
         }
       } else {
         console.log('Resubmit captcha and cookies');
-        ketqua = false;
-        json.captcha_url = ketqua;
-        res.json(json);
+        cook_capt();
+        res.jsonp(json);
       }
-      //res.json(json);     
     }
   });
 });
